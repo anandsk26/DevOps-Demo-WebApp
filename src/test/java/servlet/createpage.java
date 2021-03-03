2fabea97bc2e9754e965452ed729ec810f432831
anandsk26 <65949178+anandsk26@users.noreply.github.com>
2021-03-03 08:13:03 -0500

    54.211.8.205

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://34.207.70.108:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://54.211.8.205:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
