55a250915d93e5f903beaf549d3f217b8787916d
anandsk26 <65949178+anandsk26@users.noreply.github.com>
2021-03-03 08:42:55 -0500

    18.233.162.149

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -16,8 +16,8 @@ public class ftat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://54.211.8.205:8080/QAWebapp/";
-	public String URL1 = "http://54.211.8.205:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://18.233.162.149:8080/QAWebapp/";
+	public String URL1 = "http://18.233.162.149:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
